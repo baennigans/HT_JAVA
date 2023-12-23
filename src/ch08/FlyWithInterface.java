e968cc106989f94a8c19f0295402228514385f1f
DESKTOP-LDD4AU9\User <bgh2584@gmail.com>
2023-12-24 02:55:51 +0900

    자바 인터페이스2

---
+++ src/ch08/FlyWithInterface.java
@@ -0,0 +1,37 @@
+package ch08;
+
+public class FlyWithInterface {
+	public static void main(String[] args) {
+		Bird bird = new Bird();
+		Helicopter copter = new Helicopter();
+		Rocket rocket = new Rocket();
+		
+		Flyable[] arr = { bird, copter, rocket };
+		for(int i=0; i<arr.length; i++) {
+			arr[i].fly();
+		}
+	}
+}
+
+interface Flyable {
+	void fly();
+}
+
+class Bird implements Flyable {
+	public void fly() {
+		System.out.println("새가 날아갑니다.");
+	}
+}
+
+class Helicopter implements Flyable {
+	public void fly() {
+		System.out.println("헬리콥터가 날아갑니다.");
+	}
+}
+
+class Rocket implements Flyable {
+	public void fly() {
+		System.out.println("로켓이 날아갑니다.");
+	}
+}
+
